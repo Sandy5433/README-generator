bffb39b72024289d59d865074a84024bfa98f9c9
Sandy Hung <sandyhung83@yahoo.com.tw>
2023-01-24 21:50:53 +1100

    added function to return license link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if(license == "Apache 2.0 License"){
@@ -10,15 +10,26 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
+// Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-}
+  if(license === "Apache 2.0 License"){
+    return "https://opensource.org/licenses/Apache-2.0"
+  } else if (license == "Boost"){
+    return "https://www.boost.org/LICENSE_1_0.txt"
+  } else if (license == "No license"){
+    return ""
+}}
 
-// TODO: Create a function that returns the license section of README
+// Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
- 
+  if(license === "No license"){
+    return ""
+  } else 
+  return `## License
+  This application is licensed under ${license}, you can access the license via this link: ${renderLicenseLink(license)}
+`
 }
 
 //Create a function to generate markdown for README
