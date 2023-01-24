7d90322aab7ae34cbc00dfe942f4c7323ba8c176
Sandy Hung <sandyhung83@yahoo.com.tw>
2023-01-24 21:47:03 +1100

    added function to render badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,13 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-
+  if(license == "Apache 2.0 License"){
+    return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+  } else if (license == "Boost"){
+    return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)"
+  } else if (license == "No license"){
+    return ""
+  }
 }
 
 // TODO: Create a function that returns the license link
